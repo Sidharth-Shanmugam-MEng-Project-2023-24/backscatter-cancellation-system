a1ab48886a7ef8cf0b6e95af4351a5765c41820b
Sidharth S <sidharth.s16@yahoo.com>
2024-04-08 16:27:03 +0100

    added CaptureManager script

---
+++ app.py
@@ -0,0 +1,13 @@
+
+
+### VIDEO CAPTURE
+#   Input a directory path to feed in a series of frame images,
+#   named as an integer denoting the frame number and must be
+#   in PNG format.
+#
+#   Input a file path to feed in a video file.
+#
+#   Input integer '0' to use Picamera2 capture_array() to capture
+#   feed frame-by-frame.
+VIDEO_CAPTURE = "./export_04-08-2024-14-35-53/"
+
